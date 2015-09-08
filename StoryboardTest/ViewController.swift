748440a1a1bcd0b1f6574601da81621dbb72bb96
Hayden Moon <sohow@me.com>
2015-09-08 08:33:07 -0600

    initalize

--- StoryboardTest/ViewController.swift
+++ StoryboardTest/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonAction(sender: AnyObject) {
+        let newStroyboard = UIStoryboard(name: "Storyboard2", bundle: nil)
+        if let viewController = newStroyboard.instantiateViewControllerWithIdentifier("View1") as? UIViewController {
+            self.navigationController?.pushViewController(viewController, animated: true)
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
