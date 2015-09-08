388dd6f07ccc7cb70e75e2753750cc29ab03acc4
Hayden Moon <sohow@me.com>
2015-09-08 08:23:34 -0600

    Initial Commit

---
+++ StoryboardTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardTest
+//
+//  Created by Moon Hayden on 9/8/15.
+//  Copyright (c) 2015 Peter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
